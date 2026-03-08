e3bb3b7c0ec30691af35a4927d7918af9de45429
DL <DL@DESKTOP-16EEHN2>
2026-03-08 21:47:40 +0100

    Phase 8 : contact, detail voyage, front complet

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,13 +2,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController {
 
     #[Route('/contact', name: 'contact')]
-    public function index(): Response {
-        return $this->render("pages/contact.html.twig");
+    public function index(Request $request): Response {
+        $sent = false;
+        if ($request->isMethod('POST')) {
+            $nom = $request->request->get('nom');
+            $email = $request->request->get('email');
+            $message = $request->request->get('message');
+            $sent = true;
+        }
+        return $this->render("pages/contact.html.twig", [
+            'sent' => $sent
+        ]);
     }
 }
