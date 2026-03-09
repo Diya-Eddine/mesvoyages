87116b9fae974fb50d5f6d7e638d25d5c2054ab0
DL <DL@DESKTOP-16EEHN2>
2026-03-09 18:17:35 +0100

    Phase 19 : envoi de mails avec MailDev et Symfony Mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,24 +1,48 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController {
-
+class ContactController extends AbstractController
+{
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request): Response {
-        $sent = false;
-        if ($request->isMethod('POST')) {
-            $nom = $request->request->get('nom');
-            $email = $request->request->get('email');
-            $message = $request->request->get('message');
-            $sent = true;
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $this->sendEmail($mailer, $contact);
+            $this->addFlash('succes', 'message envoyé');
+            return $this->redirectToRoute('contact');
         }
+
         return $this->render("pages/contact.html.twig", [
-            'sent' => $sent
+            'contact' => $contact,
+            'formcontact' => $formContact
         ]);
+    }
+
+    public function sendEmail(MailerInterface $mailer, Contact $contact): void
+    {
+        $email = (new Email())
+            ->from($contact->getEmail())
+            ->to('contact@mesvoyages.com')
+            ->subject('Message du site de voyages')
+            ->html($this->renderView(
+                'pages/_email.html.twig', [
+                    'contact' => $contact
+                ]
+            ), 'utf-8');
+
+        $mailer->send($email);
     }
 }
