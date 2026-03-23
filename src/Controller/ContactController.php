00cbf2585141b710903fadb5905e8c6d914d8522
DL <DL@DESKTOP-16EEHN2>
2026-03-23 19:34:45 +0100

    Phase 24 : déploiement.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Controller;
-
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,7 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
@@ -18,31 +16,27 @@ class ContactController extends AbstractController
         $contact = new Contact();
         $formContact = $this->createForm(ContactType::class, $contact);
         $formContact->handleRequest($request);
-
         if ($formContact->isSubmitted() && $formContact->isValid()) {
             $this->sendEmail($mailer, $contact);
             $this->addFlash('succes', 'message envoyé');
             return $this->redirectToRoute('contact');
         }
-
         return $this->render("pages/contact.html.twig", [
             'contact' => $contact,
             'formcontact' => $formContact
         ]);
     }
-
     public function sendEmail(MailerInterface $mailer, Contact $contact): void
     {
         $email = (new Email())
             ->from($contact->getEmail())
-            ->to('contact@mesvoyages.com')
+            ->to('diya4345@gmail.com')
             ->subject('Message du site de voyages')
             ->html($this->renderView(
                 'pages/_email.html.twig', [
                     'contact' => $contact
                 ]
             ), 'utf-8');
-
         $mailer->send($email);
     }
 }
